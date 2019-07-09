38a285cd34d0ed64c27d0202a1328867fce23b70
Les Melnychuk <lmelnychuk@romexsoft.com>
2019-07-09 17:27:02 +0300

    Add constraints

--- Classes/GlobalStateExplorers/SQLExecutionControllers/FLEXSQLCommandExecutionViewController.m
+++ Classes/GlobalStateExplorers/SQLExecutionControllers/FLEXSQLCommandExecutionViewController.m
@@ -15,11 +15,11 @@
 @interface FLEXSQLCommandExecutionViewController ()
     @property (nonatomic) UITextView *textView;
     @property (nonatomic) UIButton *submitButton;
-    @property (nonatomic) UIStackView *stackView;
+    @property (nonatomic) UILabel *statusLabel;
 @end
 
 @implementation FLEXSQLCommandExecutionViewController
-    @synthesize isSelectionType, sql, textView, submitButton, stackView;
+    @synthesize isSelectionType, sql, textView, submitButton, statusLabel;
     
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -34,26 +34,38 @@
 - (void)addOtherUIElements {
     textView = [UITextView new];
     submitButton = [UIButton new];
+    statusLabel = [UILabel new];
+    
+    self.view.translatesAutoresizingMaskIntoConstraints = false;
+    textView.translatesAutoresizingMaskIntoConstraints = false;
+    submitButton.translatesAutoresizingMaskIntoConstraints = false;
+    statusLabel.translatesAutoresizingMaskIntoConstraints = false;
     
     [submitButton setTitle: @"Submit" forState: UIControlStateNormal];
-    [[submitButton.heightAnchor constraintEqualToConstant: 80] setActive: TRUE];
     [submitButton addTarget: self action:@selector(submitPressed) forControlEvents: UIControlEventTouchUpInside];
     
-    NSArray* constraints = @[
-        [stackView.leftAnchor constraintEqualToAnchor: self.view.leftAnchor constant: 50],
-        [stackView.rightAnchor constraintEqualToAnchor: self.view.rightAnchor constant: 50],
-        [stackView.topAnchor constraintEqualToAnchor: self.view.topAnchor constant: 50],
-        [stackView.bottomAnchor constraintEqualToAnchor: self.view.bottomAnchor constant: 50],
-    ];
+    NSLayoutConstraint* submitButtonHeight = [NSLayoutConstraint constraintWithItem: submitButton attribute: NSLayoutAttributeHeight relatedBy: NSLayoutRelationEqual toItem: nil attribute: NSLayoutAttributeNotAnAttribute multiplier: 1.0 constant: 70];
     
+    NSLayoutConstraint* statusLabelHeight = [NSLayoutConstraint constraintWithItem: statusLabel attribute: NSLayoutAttributeHeight relatedBy: NSLayoutRelationEqual toItem: nil attribute: NSLayoutAttributeNotAnAttribute multiplier: 1.0 constant: 100];
+    
+    
+    NSLayoutConstraint* textViewTopConstraint = [NSLayoutConstraint constraintWithItem: textView attribute: NSLayoutAttributeTopMargin relatedBy: NSLayoutRelationEqual toItem: self.view attribute: NSLayoutAttributeTopMargin multiplier: 1.0 constant: 0];
+    NSLayoutConstraint* textViewLeftConstraint = [NSLayoutConstraint constraintWithItem: textView attribute: NSLayoutAttributeLeftMargin relatedBy: NSLayoutRelationEqual toItem: self.view attribute: NSLayoutAttributeLeftMargin multiplier: 1.0 constant: 0];
+    NSLayoutConstraint* textViewRightConstraint = [NSLayoutConstraint constraintWithItem: textView attribute: NSLayoutAttributeRightMargin relatedBy: NSLayoutRelationEqual toItem: self.view attribute: NSLayoutAttributeRightMargin multiplier: 1.0 constant: 0];
+
+    NSLayoutConstraint* textViewBottomConstraint = [NSLayoutConstraint constraintWithItem: textView attribute: NSLayoutAttributeBottom relatedBy: NSLayoutRelationEqual toItem: statusLabel attribute: NSLayoutAttributeTop multiplier: 1.0 constant: 0];
+    NSLayoutConstraint* statusLabelBottomConstraint = [NSLayoutConstraint constraintWithItem: statusLabel attribute: NSLayoutAttributeBottom relatedBy: NSLayoutRelationEqual toItem: submitButton attribute: NSLayoutAttributeTop multiplier: 1.0 constant: 0];
+    NSLayoutConstraint* submitButtonBottomConstraint = [NSLayoutConstraint constraintWithItem: submitButton attribute: NSLayoutAttributeBottom relatedBy: NSLayoutRelationEqual toItem: self.view attribute: NSLayoutAttributeBottomMargin multiplier: 1.0 constant: 0];
+
+    NSArray* constraints = @[submitButtonHeight, statusLabelHeight, textViewTopConstraint, textViewLeftConstraint, textViewRightConstraint, textViewBottomConstraint, statusLabelBottomConstraint, submitButtonBottomConstraint];
+
     for (NSLayoutConstraint* constraint in constraints) {
         [constraint setActive: TRUE];
     }
     
-    [stackView addArrangedSubview: textView];
-    [stackView addArrangedSubview: submitButton];
-    
-    [self.view addSubview: stackView];
+    [self.view addSubview: textView];
+    [self.view addSubview: submitButton];
+    [self.view addSubview: statusLabel];
 }
     
 - (void)submitPressed {
